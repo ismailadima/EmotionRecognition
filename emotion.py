27d7b62b50d4cec15a2465f3adc0895bf309d58a
unknown <randi@labsatu.com>
2018-06-03 16:16:33 +0700

    mfcc dan target

--- emotion.py
+++ emotion.py
@@ -7,13 +7,13 @@ def avgMFCC(mfcc,kelas):
     temp = []
     for i in range(len(mfcc)):
         temp.append(np.average(mfcc[i]))
-    temp.append(kelas)
+    temp.append(kelas+1)
     return temp
 
 dataMFCC = []
 for i in range(len(label)):
-    for j in range(0,199):
-        y, sr = librosa.load("Dataset/OLDER_"+label[i]+"/older_"+label[i]+"_ ("+str(j+1)+").wav")
+    for j in range(1,201):
+        y, sr = librosa.load("Dataset/OLDER_"+label[i]+"/older_"+label[i]+"_ ("+str(j)+").wav")
         mfcc = librosa.feature.mfcc(y,sr,n_mfcc=12)
         mfccs = avgMFCC(mfcc,i)
         dataMFCC.append(mfccs)
